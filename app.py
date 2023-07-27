5ff4542d61cb2046352156eed031248b9f48b99e
Ravikishore31 <rkishore.adabala@gmail.com>
2023-07-27 15:51:24 +0530

    4th commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Hello, Welcome to the Demo Project of AWS Codepipeline1'
 
 if __name__ == '__main__':
     app.run()
