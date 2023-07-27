08abea47b3c986b63a91d4852ace6d43e0a0175b
ADABALA RAVIKISHORE <rkishore.adabala@gmail.com>
2023-07-27 12:14:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Hello, Welcome to the Demo Project of AWS Codepipeline'
 
 if __name__ == '__main__':
     app.run()
